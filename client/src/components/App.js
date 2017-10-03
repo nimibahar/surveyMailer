a7f05eaedc4d040f6b979efb29ce647e0cb39b95
Nimrod Bachar <nimrodbachar@Nimrods-MacBook-Pro.local>
2017-10-03 12:26:12 +0200

    include materialize-css to the client

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-const Header = () => <h2>Header</h2>;
+import Header from './Header';
+
 const Dashboard = () => <h2>Dashboard</h2>;
 const NewSurvey = () => <h2>NewSurvey</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/surveys" component={Dashboard} />
-        <Route path="/surveys/new" component={NewSurvey} />
-      </div>
-    </BrowserRouter>
+    <div className="container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={NewSurvey} />
+        </div>
+      </BrowserRouter>
+    </div>
   );
 };
 
